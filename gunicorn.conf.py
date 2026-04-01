e1df6c52db2f41ced0e8ec8e6c340e5d6d3d5b9a
Aymen022 <78912492+Aymen022@users.noreply.github.com>
2026-04-01 23:34:40 +0000

    up

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:5000")
-workers = int(os.environ.get("GUNICORN_WORKERS", "4"))
-timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
-accesslog = "-"
-errorlog = "-"
-loglevel = os.environ.get("LOG_LEVEL", "info").lower()
--- run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-cxn_app = create_app()
-
-if __name__ == "__main__":
-    cxn_app.run(host="0.0.0.0", port=5000)
